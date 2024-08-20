b1772ab2648f5e726cb5fb7e0264a277707da7bd
apzkxkdn <impsim@naver.com>
2024-08-20 18:34:46 +0900

    ㅁㅁ

--- BookManager/src/Managers/ShowManager.java
+++ BookManager/src/Managers/ShowManager.java
@@ -10,8 +10,14 @@ public class ShowManager {
 		return instance;
 	}
 	
+	public void inputCursor()
+	{
+		System.out.println("입력 해주세요 : ");
+	}
+	
 	public void menuShow()
 	{
 		System.out.println("1. 도서 리스트 확인 2. 도서 등록 3. 도서 수정 4. 도서 검색 \n5.나가기");
+		inputCursor();
 	}
 }
